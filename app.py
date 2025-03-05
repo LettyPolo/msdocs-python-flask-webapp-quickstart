fa0b9c1eba08e5dd434eb3b04daa59b12ceb2ba9
Letty Polo <letty.polo@outlook.com>
2025-03-05 23:41:19 +0100

    shopping cart and confirm

--- app.py
+++ app.py
@@ -16,18 +16,24 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/add_to_cart_page', methods=['POST'])
-def hello():
+@app.route('/dd_to_cart_page', methods=['POST'])
+def dd_to_cart_page():
    p_name = request.form.get('p_name')
    p_quantity = request.form.get('p_quantity')
    total_value = request.form.get('total_value')
+   
+   return render_template('dd_to_cart_page.html', p_name = p_name, p_quantity=p_quantity,total_value=total_value)
 
-   if p_name:
-       print('Request for hello page received with name=%s' % p_name)
-       return render_template('dd_to_cart_page.html', p_name = p_name, p_quantity=p_quantity,total_value=total_value)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+
+
+@app.route('/confirm', methods=['POST'])
+def confirm():
+   p_name = request.form.get('p_name')
+   p_quantity = request.form.get('p_quantity')
+   total_value = request.form.get('total_value')
+   
+   return render_template('confirm.html', p_name = p_name, p_quantity=p_quantity,total_value=total_value)
+
 
 
 if __name__ == '__main__':
